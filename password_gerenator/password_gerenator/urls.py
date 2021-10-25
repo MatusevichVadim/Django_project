429393092df98a407344fdaf150e506b1e9da0b1
Elmooo <Matusevichfizika@yandex.ru>
2021-10-25 21:57:02 +0300

    2 commit 25.10.2021 21:56

--- password_gerenator/password_gerenator/urls.py
+++ password_gerenator/password_gerenator/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
-    path('lol', views.lol),
+    path('home_pages/', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('description/', views.description, name='description'),
 ]
