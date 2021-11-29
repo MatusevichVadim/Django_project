ef3720991c40e84a65059b61d335135c42600669
Elmooo <Matusevichfizika@yandex.ru>
2021-11-29 22:22:58 +0300

    12 cmt 29.11 creating tasks, viewing and updating

--- todowoo_project/todo/views.py
+++ todowoo_project/todo/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect # рендесоздание страницы, редирект-переход на другую страницу
+from django.shortcuts import render, redirect, get_object_or_404 # рендеh-создание страницы, редирект-переход на другую страницу,get для поиска ключа в бд
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm #формы для создания и аутентификации юзера
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate #методы аутентификации
 from .forms import TodoForm #форма для создания туду
+from .models import Todo #модель туду
+from django.utils import timezone #для заполнения поля datecomplete, для выполнения туду в функции completetodo
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -62,4 +64,34 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'error переданы не верные данные'} )
 
 def currenttodo(request):
-    return render(request, 'todo/currenttodo.html')
+    todos = Todo.objects.filter(user=request.user, datecomplited__isnull=True) #обект словаря из моделей filter(user=request.user)-
+    # -для вывода записей пользователя. datecomplited__isnull=True для отображения выполненных туду, если поле заполнено, отображать не будет
+    return render(request, 'todo/currenttodo.html', {'todos': todos})
+
+def viewtodo(request, todo_pk): #ключ записи для отображении на странице
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user) #класс и ключ
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)  # форма для изменения уже сущевствующей туду
+        return render(request, 'todo/viewtodo.html',
+                      {'todo': todo, 'form': form})  # передаем ещё и форму для возможного изменения
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo) #без instance=туду бд будет думать что мы создаем новую запись а не изменяем существующуючё
+            form.save()
+            return redirect('currenttodo')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html',
+                          {'todo': todo, 'form': form, 'error': 'плохая информация'})
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST': #запрос на выполнение
+        todo.datecomplited = timezone.now() #заполенение поля временем для отметки выполнено
+        todo.save()
+        return redirect('currenttodo')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST': #запрос на удаление
+        todo.delete()
+        return redirect('currenttodo')
