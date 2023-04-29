53939f1bb7a17a1f66f707dc5dc47c1e7773fd27
Elmo00 <Matusevichfizika@yandex.ru>
2023-04-29 20:53:56 +0300

    added register students, content access, drawing different types of content 662
    Signed-off-by: Elmo00 <Matusevichfizika@yandex.ru>

--- E_learning_platform/config/settings.py
+++ E_learning_platform/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'embed_video',
+
     'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +73,10 @@ TEMPLATES = [
         },
     },
 ]
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.template.context_processors.request',
+)
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
@@ -132,3 +140,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
--- E_learning_platform/courses/urls.py
+++ E_learning_platform/courses/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('mine/', views.ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
@@ -14,16 +13,10 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/<id>/',
          views.ContentCreateUpdateView.as_view(),
          name='module_content_update'),
-    path('content/<int:id>/delete/',
-         views.ContentDeleteView.as_view(),
-         name='module_content_delete'),
-    path('module/<int:module_id>/',
-         views.ModuleContentListView.as_view(),
-         name='module_content_list'),
-    path('module/order/',
-         views.ModuleOrderView.as_view(),
-         name='module_order'),
-    path('content/order/',
-         views.ContentOrderView.as_view(),
-         name='content_order'),
+    path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name='module_content_delete'),
+    path('module/<int:module_id>/', views.ModuleContentListView.as_view(), name='module_content_list'),
+    path('module/order/', views.ModuleOrderView.as_view(), name='module_order'),
+    path('content/order/', views.ContentOrderView.as_view(), name='content_order'),
+    path('subject/<slug:subject>/', views.CourseListView.as_view(), name='course_list_subject'),
+    path('<slug:slug>/', views.CourseDetailView.as_view(), name='course_detail')
 ]
