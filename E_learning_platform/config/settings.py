c583a7f3942ccf7135b2a5613398e542e5cacd75
Elmo00 <Matusevichfizika@yandex.ru>
2023-04-30 14:49:12 +0300

    added RESTful API for courses 701
    Signed-off-by: Elmo00 <Matusevichfizika@yandex.ru>

--- E_learning_platform/config/settings.py
+++ E_learning_platform/config/settings.py
@@ -16,7 +16,6 @@ from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-=a+56mn&cec_*qtlz+dlj4g!u@tyijuhw!=t)_i3z66fw*1y60
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -42,6 +40,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'redisboard',
     'embed_video',
+    'rest_framework',
 
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
@@ -83,7 +82,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -99,6 +97,12 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSIONS_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 # Password validation
@@ -119,7 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -130,7 +133,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -149,7 +151,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
---
+++ E_learning_platform/courses/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+app_name: str = 'courses'
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
+urlpatterns = [
+    path('subjects/', views.SubjectListView.as_view(), name='subject/<pk>/'),
+    path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
+    # path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll'),
+    path('', include(router.urls))
+]
