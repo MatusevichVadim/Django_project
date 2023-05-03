692b5c287e647a01d853439a577ce4542b198f03
Elmo00 <Matusevichfizika@yandex.ru>
2023-05-03 20:18:27 +0300

    added django-postgresql-redis docker compose 7.5.5
    Signed-off-by: Elmo00 <Matusevichfizika@yandex.ru>

---
+++ E_learning_platform/config/settings/prod.py
@@ -0,0 +1,25 @@
+import os
+from .base import *
+
+DEBUG = False
+
+ADMINS = [
+    ('Matusevich Vadim', 'Matusevichfizika@yandex.by'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_NAME'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
