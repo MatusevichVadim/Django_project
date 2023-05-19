9907c4ed0a55942e154c5b6c4d3c505a6e79fcb5
Elmo00 <Matusevichfizika@yandex.ru>
2023-05-19 17:44:01 +0300

    add the blog

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
+from .forms import EmailPostForm
+
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -30,3 +40,16 @@ def post_detail(request, year, month, day, post):
         'post': post
     }
     return render(request, 'blog/post/detail.html', context)
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm
+    context = {'post': post,
+               'form': form}
+    return render(request, 'blog/post/share.html', context)
